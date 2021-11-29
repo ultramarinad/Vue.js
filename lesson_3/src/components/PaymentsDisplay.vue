f4f2ef1945a8f4be202e866f597d2c85b1a1457d
Alexander <saniele22@gmail.com>
2021-11-29 22:05:11 +0700

    Lesson_3

---
+++ lesson_3/src/components/PaymentsDisplay.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="payments-display-wrapper">
+    <table class="payments-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Category</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, idx) in items" :key="idx">
+          <td>{{ idx + 1 }}</td>
+          <td>{{ item.date }}</td>
+          <td>{{ item.category }}</td>
+          <td>{{ item.value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentsDisplay",
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.payments-display-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.payments-table {
+  width: 500px;
+  border-collapse: collapse;
+  & th {
+    font-weight: bold;
+    padding: 10px;
+    border-bottom: 1px solid rgb(209, 206, 206);
+  }
+  & td {
+    padding: 10px;
+    border-bottom: 1px solid rgb(209, 206, 206);
+  }
+}
+</style>
